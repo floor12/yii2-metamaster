b586ab8a5e1d1357f173e2f04196f0257bafb27f
floor12 <floor12@floor12.net>
2018-10-29 20:03:32 +0100

    Setter for og:type meta tag.

--- src/MetaMaster.php
+++ src/MetaMaster.php
@@ -50,8 +50,6 @@ class MetaMaster extends Component
         parent::init();
     }
 
-
-
     /** Page title setter
      * @param $title
      * @return $this
@@ -61,17 +59,6 @@ class MetaMaster extends Component
         $this->title = $title;
         return $this;
     }
-
-    /** OgType setter
-     * @param $type
-     * @return $this
-     */
-    public function setType(string $type)
-    {
-        $this->type = $type;
-        return $this;
-    }
-
 
     /** OgType setter
      * @param $type
